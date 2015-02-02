fd473a44718f96dc9ef4568bb5e06dd037a397d3
jaudas <jessica.audas@epfedu.Fr>
2015-02-02 17:56:42 +0100

    modif setQualifieOffice

--- src/controleur/TourEliminatoire.java
+++ src/controleur/TourEliminatoire.java
@@ -8,46 +8,31 @@ import model.Match;
 public class TourEliminatoire {
 	private LinkedList<Equipe> listeEquipesTour;
 	private LinkedList<Match> listeMatchTour;
-	int qualifOffice;
+	int qualifOffice = -1;
 	
 	public TourEliminatoire()
 	{
 		super();
-		qualifOffice = -1;
 	}
 	
-	
+	public int setQualifOffice()
+	{
+		int indexEquipe = 0;
+		indexEquipe = (int) (Math.random()*listeEquipesTour.size());	
+		return indexEquipe;
+		
+	}
 	public void CreerTour()
 	{
-		/*UTILISER PLUtOT LE DERNIER = LE QUALIFIE DOFFICE
-		if (listeEquipesTour.size()%2 != 0)
-		{
-			qualifOffice = (int) (Math.random()*listeEquipesTour.size()); 
-		}
+		int j = listeEquipesTour.size();
 		int i = 0;
-		while(i<(int)listeEquipesTour.size())
+		
+		if (listeEquipesTour.size()%2 !=0)
 		{
-			if(i!=qualifOffice && i+1 != qualifOffice)
-			{
-				Match m = new Match(listeEquipesTour.get(i),listeEquipesTour.get(i+1)); 
-				listeMatchTour.add(m);
-				i=+2;
-			}
-			else if (i==qualifOffice)
-			{
-				Match m = new Match(listeEquipesTour.get(i+1),listeEquipesTour.get(i+2)); 
-				listeMatchTour.add(m);
-				i=+3;
-			}
-			else if (i+1==qualifOffice)
-			{
-				Match m = new Match(listeEquipesTour.get(i),listeEquipesTour.get(i+2)); 
-				listeMatchTour.add(m);
-				i=+3;
-			}
-			
-			
-		}*/
+			qualifOffice = setQualifOffice();
+		}
+		
+		
 	}
 	
 
