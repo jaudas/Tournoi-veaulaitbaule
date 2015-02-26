0368db39e8a827b34f36e2765a77d6bce52157f5
jaudas <jessica.audas@epfedu.Fr>
2015-02-26 16:41:33 +0100

    save listeEquipes

--- src/model/SaveList.java
+++ src/model/SaveList.java
@@ -1,60 +1,102 @@
 package model;
 
-
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.text.ParseException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import model.Equipe;
+import view.ConsoleView;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import controleur.InfoEquipes;
 
 public class SaveList {
 
-	JSONParser parser = new JSONParser();
-
-	public static void save(String nomListe, LinkedList <Equipe> listToSave) throws IOException, org.json.simple.parser.ParseException, ParseException
+	public static void save(String nomListe, LinkedList <Equipe> listToSave) throws IOException
 	{
-		JSONObject obj = new JSONObject();
-		//JSONArray list = (JSONArray)listToSave;
+		System.out.println("--- Serialize Started ---");
+        String fileNameHisto = System.getProperty("user.dir")
+        		+ "//src//data//historique//"+nomListe+".json";
+        System.out.println("enregistré dans : " + fileNameHisto);
+        
 
-		//Nom de la liste + liste d'équipe
-		for(int i=0; i<listToSave.size();i++)
-		{
-			obj.put("Nom :",listToSave.get(i).getNom());
-			obj.put("Descriptions :", listToSave.get(i).getDescription());
-			obj.put("Joueurs : ", listToSave.get(i).getListeJoueurs());
-			obj.put("Nombre de matchs joués", listToSave.get(i).getHistoNbMatchJoue());
-			obj.put("Nombre de Victoires", listToSave.get(i).getHistoNbVictoire());
-			obj.put("Nombre de Sets Gagnés", listToSave.get(i).getHistoNbSetGagne());
-			obj.put("Nombre de Sets Perdus", listToSave.get(i).getHistoNbSetPerdu());
-		}
+        Gson gson = new Gson();
+       // Type listOfTestObject = new TypeToken<List<Equipe>>(){}.getType();
+
+        //Make Serial
+        System.out.println("test 2");
+
+        Writer osWriter = new OutputStreamWriter( new FileOutputStream(fileNameHisto));
+        
+        //FileOutputStream output = new FileOutputStream(fileNameHisto);
+        //Writer osWriter = new OutputStreamWriter(output);
+ 
+        System.out.println("liste size : " + listToSave.size());
+
+		gson.toJson(Collections.synchronizedList(listToSave), osWriter);
+		osWriter.close();
+		
+		System.out.println("Fin Serialize");
 		
 		
+		
+	}
+	
+	public static LinkedList<Equipe> loadTeams (String nomListe) throws IOException
+	{
+		System.out.println("Deserialize Started");
+        String fileNameHisto = System.getProperty("user.dir")
+        		+ "//src//data//historique//"+nomListe+".json";
+        System.out.println("enregistré dans : " + fileNameHisto);
+        
+        Gson gson = new Gson();
+       
+        Type listOfTestObject = new TypeToken<LinkedList<Equipe>>(){}.getType();
+		//Eat Serial
+        
+        Reader isReader = new InputStreamReader(new FileInputStream(fileNameHisto));
+        LinkedList<Equipe> listFromJson = gson.fromJson(isReader, listOfTestObject);
+		List<Equipe> list2 = Collections.synchronizedList(listFromJson);
+        isReader.close();
+        
+        LinkedList<Equipe> listeEquipes = new LinkedList<Equipe>();
+        
+        //On met les éléments de la List dans la LinkedList
+        for (int i = 0; i<list2.size();i++)
+        	listeEquipes.add(list2.get(i));
+        
+        System.out.println("Deserialize Ended ");
+        
+        return listeEquipes;
+        
+	}
 
+	public static void main(final String[] args) throws IOException{
+		LinkedList<Equipe> liste = InfoEquipes.inscrireEquipes();
 
-		StringWriter out = new StringWriter();
-		obj.writeJSONString(out);
-
- 		String jsonText = obj.toJSONString();
-		System.out.print(jsonText);
-
-
-		try {
-
-			FileWriter file = new FileWriter(System.getProperty("user.dir")+ "//src//data//historique//"+nomListe+".json");
-			file.write(obj.toJSONString());
-			file.flush();
-			file.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		String s = "patate";
+		save(s,liste);
+		
+		LinkedList<Equipe> listeapres = new LinkedList<Equipe>();
+		
+		listeapres = loadTeams(s);
+		
+		System.out.println("Avant : " );
+		
+		ConsoleView.afficherEquipesEtJoueurs(liste);
+		System.out.println("Après : ");
+	
+		ConsoleView.afficherEquipesEtJoueurs(listeapres);
+		
 	}
 
 }
